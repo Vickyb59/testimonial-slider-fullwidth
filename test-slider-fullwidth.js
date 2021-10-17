b976d5461424ac3dcf161525934d3244766d7079
crystabelleb <87649148+crystabelleb@users.noreply.github.com>
2021-10-17 18:11:44 +0100

    edit

--- test-slider-fullwidth.js
+++ test-slider-fullwidth.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $(".testimonial-carousel").slick({
     infinite: true,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     arrows: true,
     prevArrow: $(".testimonial-carousel-controls .prev"),
     nextArrow: $(".testimonial-carousel-controls .next")
